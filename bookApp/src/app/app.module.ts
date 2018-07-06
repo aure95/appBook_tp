073b3c0a5762447885558507bda43e5194bb53b5
aure <mouretaurelien95git@gmail.com>
2018-07-06 13:00:11 +0200

    manger

--- bookApp/src/app/app.module.ts
+++ bookApp/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DetailBookComponent } from './detail-book/detail-book.component';
+import { BookComponent } from './book/book.component';
+import {BookService} from './services/book.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailBookComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
